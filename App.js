7198dfd7462b0025e2b068595d26e8e17b83dbbe
Rafael Selvagio <rafael.selvagio@gmail.com>
2024-02-06 09:36:13 -0300

    AppLogin

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, ScrollView, Platform, Alert } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.content}>
+          <Image style={styles.logo} source={require('./assets/senai_logo.png')} />
+          <TextInput
+            style={styles.input}
+            placeholder="Nome de Usuário"
+            value={username}
+            onChangeText={text => setUsername(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={text => setPassword(text)}
+          />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2', // Fundo cinza
+    height: '100%', // Ajuste para ocupar toda a altura
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: 300, // Ajuste a largura conforme necessário
+    height: 80, // Ajuste a altura conforme necessário
+    marginBottom: 20,
+  },
+  content: {
+    width: '100%',
+    alignItems: 'center', // Centraliza os itens horizontalmente
+  },
+  input: {
+    height: 40,
+    width: '90%', // Alterado para ocupar 90% horizontal
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
   },
 });
+
+export default App;
